70b6d3ff618d3972765c2cacfa6b50dcfd0eca8b
ngbricen <bricen@gmail.com>
2017-10-06 15:53:14 -0400

    Changing static folder

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const PORT = process.env.PORT || 3001;
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 // Serve up static assets
-app.use(express.static("client/build"));
+app.use(express.static("public"));
+
 // Add routes, both API and view
 app.use(routes);
 
